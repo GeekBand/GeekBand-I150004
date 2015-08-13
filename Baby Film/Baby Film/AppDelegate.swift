9b7a6326ec353ee95604c4e66f7772d45de97e3a
gewill <531sunlight@gmail.com>
2015-08-13 08:38:35 +0800

    update Baby Film photos

--- Baby Film/Baby Film/AppDelegate.swift
+++ Baby Film/Baby Film/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Baby Film
 //
-//  Created by Will Ge on 8/12/15.
-//  Copyright (c) 2015 gewill.org. All rights reserved.
+//  Created by Will Ge on 8/13/15.
+//  Copyright © 2015 gewill.org. All rights reserved.
 //
 
 import UIKit
