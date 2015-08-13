707048b36857b5dfd9237077fe3af971d17b2bd1
gewill <531sunlight@gmail.com>
2015-08-13 08:03:16 +0800

    add Baby Film

---
+++ Baby Film/ObjcUML/origin.js
@@ -0,0 +1,12 @@
+   var dependencies = {
+    links:
+      [
+            { "source" : "AppDelegate", "dest" : "_TtC9Baby_Film11AppDelegate" },
+            { "source" : "GWBaseViewController", "dest" : "_TtC9Baby_Film20GWBaseViewController" },
+            { "source" : "GWCameraViewController", "dest" : "_TtC9Baby_Film22GWCameraViewController" },
+            { "source" : "GWFilmViewController", "dest" : "AVPlayer" },
+            { "source" : "GWFilmViewController", "dest" : "AVPlayerViewController" },
+            { "source" : "GWFilmViewController", "dest" : "_TtC9Baby_Film20GWFilmViewController" },
+         ]
+    }
+  ;
