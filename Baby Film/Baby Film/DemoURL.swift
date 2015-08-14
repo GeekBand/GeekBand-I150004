8c54b9d9db755079306dd63eee90f19564c3ba08
gewill <531sunlight@gmail.com>
2015-08-14 09:20:39 +0800

    update Baby Film with Auto Lyayout

---
+++ Baby Film/Baby Film/DemoURL.swift
@@ -0,0 +1,17 @@
+//
+//  DemoURL.swift
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "http://c.hiphotos.baidu.com/image/pic/item/0b7b02087bf40ad1f84a7513542c11dfa8ecce90.jpg")
+    struct NASA {
+        static let Cassini = NSURL(string: "http://c.hiphotos.baidu.com/image/pic/item/58ee3d6d55fbb2fb31d7af464d4a20a44723dce2.jpg")
+        static let Earth = NSURL(string: "http://e.hiphotos.baidu.com/image/pic/item/d043ad4bd11373f03601622aa60f4bfbfbed044c.jpg")
+        static let Liuwen = NSURL(string: "http://img4.duitang.com/uploads/item/201505/08/20150508004226_ca5WA.jpeg")
+    }
+}
---
+++ Baby Film/Baby Film/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let ivc = segue.destinationViewController as? ImageViewController {
+            if let identifier = segue.identifier {
+                switch identifier {
+                case "Earth":
+                    ivc.imageURL = DemoURL.NASA.Earth
+                    ivc.title = "Earth"
+                case "Liuwen":
+                    ivc.imageURL = DemoURL.NASA.Liuwen
+                    ivc.title = "Saturn"
+                case "Cassini":
+                    ivc.imageURL = DemoURL.NASA.Cassini
+                    ivc.title = "Cassini"
+                default: break
+                }
+            }
+        }
+    }
+    
+}
+
